bf53954c7fcc058dcc73e4330a02f45399f4aa7d
Adeel Malik <amalik@datical.com>
2024-01-08 12:51:29 -0600

    moved flow files into flowfiles directory

--- runme.sh
+++ runme.sh
@@ -1,7 +1,7 @@
 # Setting env vars
 export TAG=`date +"%Y%m%d"`
 export LBCOMMAND="$1"
-export COMMAND_FLOW_FILE="liquibase.flowfile-${LBCOMMAND}.yaml"
+export COMMAND_FLOW_FILE="flowfiles/liquibase.flowfile-${LBCOMMAND}.yaml"
 export BYPASS_LIQUIBASE_CHECKS="$2"
 
 # Setting Liquibase command args
@@ -16,9 +16,12 @@ export LIQUIBASE_COMMAND_CHECKS_SCOPE="changelog,database"
 export LIQUIBASE_LIQUIBASE_SCHEMA_NAME="LIQUIBASE_USER"
 
 # Setting reporting variables
-export LIQUIBASE_REPORTS_ENABLED="true"
-export LIQUIBASE_REPORTS_NAME="update_report.html"
 export LIQUIBASE_REPORTS_PATH="reports"
+# export LIQUIBASE_REPORTS_ENABLED="true"
+export LIQUIBASE_COMMAND_UPDATE_REPORT_ENABLED="true"
+# export LIQUIBASE_REPORTS_NAME="update_report.html"
+export LIQUIBASE_COMMAND_UPDATE_REPORTS_NAME="update_report.html"
+
 
 
 
@@ -28,4 +31,4 @@ export PATH=/Users/adeelmalik/Desktop/LiquibaseHub/liquibase-4.25.1:${PATH}
 # Run flow file ...
 echo TAG is ${TAG}
 echo LBCOMMAND is ${LBCOMMAND}
-liquibase flow --flow-file=liquibase.flowfile-cd.yaml
+liquibase flow --flow-file=flowfiles/liquibase.flowfile-cd.yaml
